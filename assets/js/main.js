876c924c6d6bc55836a3fad350f9dc18d8b7ab66
rositadiazgalaz <rositadiazgalaz@gmail.com>
2017-06-18 21:32:15 -0400

    comprendiendo dragstart, dragend, y grag

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
-main.js
+function comenzar(){
+
+var elemOrigen = document.getElementById("imagen"); 
+/* dragstart se desencadena cuando se comienza a 
+arrastrar un objeto */
+//ejemplo
+/*elemOrigen.addEventListener("dragstart", function(){
+	alert("comenzó el evento");
+}, false);
+
+/* dragend se desencadena cuando se termina de arrastrar
+ el objeto independiente de donde se suelte el objeto 
+  y con drag se dispara mientras esté en movimiento el
+   objeto*/
+   // ejemplo
+/*elemOrigen.addEventListener("dragend", function(){
+	alert("se termino de arrastrar");
+}, false); */
+
+}
+
+
+
+
+window.addEventListener("load", comenzar, false);
